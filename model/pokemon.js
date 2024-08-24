d98e331e2e340659ca729b20d947861bdcf21869
TRGRally <59124512+TRGRally@users.noreply.github.com>
2024-08-24 17:54:43 +0100

    set up indexedDB

--- model/pokemon.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=pokemon.js.map
--- util/repository.ts
+++ util/repository.ts
@@ -5,8 +5,19 @@ enum BaseURL {
 const pokemonData = await fetchPokemonJson(); 
 const regionForms = extractRegionForms(pokemonData);
 const allPokemonData = pokemonData.concat(regionForms);
+const allPokemonArray = getPokemonArray(allPokemonData);
 
-export const allPokemon = getPokemonArray(allPokemonData);
+//map of all pokemon by formid -> pokemon
+//nidoran male and female have the same formid but belonging to a different pokemon id. 
+//this is the only non-unique formid across all pokemon ids.
+//basically formid == uniqueid if you ignore nidoran. its irrelevant anyway.
+
+export const allPokemonMap = new Map<string, Pokemon>();
+
+allPokemonArray.forEach((pokemon) => {
+    allPokemonMap.set(pokemon.formId, pokemon);
+});
+
 
 export async function get(apiUrl: string): Promise<any> {
 
@@ -179,4 +190,94 @@ function getPokemonArray(pokemonJsonArray: PokemonJson[]): Pokemon[] {
     });
 
     return pokemonArray;
+}
+
+
+
+const indexedDB = window.indexedDB;
+
+const request = indexedDB.open('pokedex', 3);
+
+request.onerror = (event) => {
+    console.error('Database error:', event);
+};
+
+request.onupgradeneeded = (event) => {
+    console.warn('Database upgrade needed:', event);
+    const db = request.result;
+
+    if (db.objectStoreNames.contains('pokemon')) {
+        db.deleteObjectStore('pokemon');
+    }
+
+    const store = db.createObjectStore('pokemon', { keyPath: 'formId' });
+
+    store.createIndex("id", "id", { unique: false });
+    store.createIndex("name", "name", { unique: false });
+    store.createIndex("type1", "type1", { unique: false });
+    store.createIndex("type2", "type2", { unique: false });
+    store.createIndex("generation", "generation", { unique: false });
+    store.createIndex("dexNr", "dexNr", { unique: false });
+}
+
+request.onsuccess = (event) => {
+    const db = request.result;
+    const transaction = db.transaction('pokemon', 'readwrite');
+
+    const store = transaction.objectStore('pokemon');
+
+    const nameIndex = store.index('name');
+    const idIndex = store.index('id');
+    const type1Index = store.index('type1');
+    const type2Index = store.index('type2');
+    const generationIndex = store.index('generation');
+    const dexNrIndex = store.index('dexNr');
+
+    const allPokemonArray = Array.from(allPokemonMap.values());
+
+    allPokemonArray.forEach((pokemon) => {
+        store.put(pokemon);
+    });
+
+    const formIDQuery = store.get('CHARIZARD');
+    const type1Query = type1Index.getAll('POKEMON_TYPE_STEEL');
+    const type2Query = type2Index.getAll('POKEMON_TYPE_STEEL');
+    const idQuery = idIndex.getAll('MEOWTH'); //has two regional forms + orginal
+    const generationQuery = generationIndex.getAll(1);
+    const dexNrQuery = dexNrIndex.getAll(201); // unown has a bunch of forms
+
+    console.log("poo")
+
+    formIDQuery.onsuccess = (event) => {
+        console.log('FormID query:', formIDQuery.result);
+    }
+
+    type1Query.onsuccess = (event) => {
+        console.log('Type1 query (POKEMON_TYPE_STEEL):', type1Query.result);
+    }
+
+    type2Query.onsuccess = (event) => {
+        console.log('Type2 query (POKEMON_TYPE_STEEL):', type2Query.result);
+    }
+
+    idQuery.onsuccess = (event) => {
+        console.log('ID query:', idQuery.result);
+    }
+
+    generationQuery.onsuccess = (event) => {
+        console.log('Generation query:', generationQuery.result);
+    }
+
+    dexNrQuery.onsuccess = (event) => {
+        console.log('DexNr query:', dexNrQuery.result);
+    }
+
+    transaction.oncomplete = (event) => {
+        db.close();
+    }
+
+    transaction.onerror = (event) => {
+        console.error('Transaction error:', event);
+    }
+
 }
